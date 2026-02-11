e440c4eb8773d4f443d0da0d51348f90988b6066
ducmanhtechdev <supermoffcial@gmail.com>
2026-02-11 15:17:33 +0700

    61-70

---
+++ 61-70/Exercise68.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    bool check = false;
+    long long sum = 0;
+    int count = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            check = true;
+            sum += i;
+            count++;
+        }
+    }
+    if (check == false)
+    {
+        cout << 0;
+        return 0;
+    }
+    cout << sum / count;
+
+    return 0;
+}
---
+++ 61-70/Exercise69.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    bool check = false;
+    long long sum = 0;
+    int count = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 5 == 0)
+        {
+            check = true;
+            sum += i;
+            count++;
+        }
+    }
+    if (check == false)
+    {
+        cout << 0;
+        return 0;
+    }
+    cout << fixed << setprecision(1) << sum / (double)count;
+
+    return 0;
+}
